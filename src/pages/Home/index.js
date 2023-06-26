bd9316d5d6dda231f896fd2f38e63e8def03351c
Epouvaantail <coutouzis.kilian@gmail.com>
2023-06-26 11:32:19 +0200

    change a const

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -13,8 +13,8 @@ import Modal from "../../containers/Modal";
 import { useData } from "../../contexts/DataContext";
 
 const Page = () => {
-  const {last} = useData();
-  // const last = data?.events.sort((evtA, evtB) => (new Date(evtA.date) < new Date(evtB.date) ? 1 : -1));
+  const {data} = useData();
+  const last = data && data.events.sort((a, b) => new Date(b.date) - new Date(a.date))[0];
   return <>
     <header>
       <Menu />
